7217a7c970dece92796b3b6d906aa44753c827ae
Raymond Chan <am5a03@gmail.com>
2019-03-02 13:35:29 +0800

    Added dagger, added api key

--- app/src/main/java/com/raymondctc/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/com/raymondctc/udacity/popularmovies/MainActivity.java
@@ -3,11 +3,13 @@ package com.raymondctc.udacity.popularmovies;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends DaggerAppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
